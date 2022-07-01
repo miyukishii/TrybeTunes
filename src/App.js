910f5d3c1c9f456a2b9a347dde0462a1a3405507
Miyuki Ishii <miyukisabrina@gmail.com>
2022-07-01 15:36:57 -0300

    1 Requirement - Creating necessary routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route path="/search" component={ Search } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route
+          path="/album/:id"
+          render={ (props) => <Album { ...props } /> }
+        />
+        <Route exact path="/" component={ Login } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
