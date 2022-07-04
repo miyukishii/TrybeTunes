f0ffcb8ce0b5fc750b758c86bea759dbbb4f8ea3
Miyuki Ishii <miyukisabrina@gmail.com>
2022-07-04 14:04:19 -0300

    5 Requirement - Creating form for searching

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,59 @@ import React from 'react';
 import Header from '../componentes/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchinput: '',
+      buttonDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      searchinput: target.value,
+    }, this.verifyInput({ target }));
+  }
+
+  verifyInput = ({ target }) => {
+    const minInputLength = 2;
+    if (target.value.length >= minInputLength) {
+      this.setState({
+        buttonDisabled: false,
+      });
+    } else {
+      this.setState({
+        buttonDisabled: true,
+      });
+    }
+  }
+
   render() {
+    const { searchinput, buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>componente Search</p>
+        <form>
+          <label htmlFor="searchinput">
+            Name
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              id="searchinput"
+              name="searchinput"
+              value={ searchinput }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ buttonDisabled }
+            onClick={ () => {} }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
