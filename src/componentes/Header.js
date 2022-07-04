cfc5222d390da46c678ebde2d7129d1dcde92c7f
Miyuki Ishii <miyukisabrina@gmail.com>
2022-07-04 13:41:56 -0300

    4 Requirement - Creating links in Header and fixing lint problems

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -31,7 +32,17 @@ class Header extends React.Component {
       <header data-testid="header-component">
         {loading && <Loading />}
         {user
-        && <h2 data-testid="header-user-name">Olá, {user} :)</h2>}
+        && (
+          <h2 data-testid="header-user-name">
+            Olá,
+            { user }
+            !
+          </h2>)}
+        <nav className="nav-bar">
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
       </header>
     );
   }
